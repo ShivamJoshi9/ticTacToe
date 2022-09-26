6e9af336dcbb4c4a89f8a40c13822faa1d41ca39
Shivam Joshi <95294234+ShivamJoshi9@users.noreply.github.com>
2022-09-26 10:56:11 +0530

    Add files via upload
    tic-Tac-Toe finished

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -14,22 +14,24 @@ const changeTurn = () => {
 
 // Function to check for a win
 const checkWin = () => {
-    let boxText = document.getElementsByClassName('boxText');
+    let boxtext = document.getElementsByClassName('boxText');
     let wins = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
+        [0, 1, 2, 5, 5, 0],
+        [3, 4, 5, 5, 15, 0],
+        [6, 7, 8, 5, 25, 0],
+        [0, 3, 6, -5, 15, 90],
+        [1, 4, 7, 5, 15, 90],
+        [2, 5, 8, 15, 15, 90],
+        [0, 4, 8, 5, 15, 45],
+        [2, 4, 6, 5, 15, 135],
     ]
     wins.forEach(e => {
-        if ((boxText[e[0]].innerText === boxText[e[1]].innerText) && (boxText[e[2]].innerText === boxText[e[1]].innerText) && (boxText[e[0]].innerText !== "")) 
-        {
-                    document.querySelector('.info').innerText = boxText[e[0]].innerText + "won"
-                    isGameOver = true 
+        if ((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "")) {
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
+            isGameOver = true;
+            document.querySelector('.imgBox').getElementsByTagName('img')[0].style.width = "250px";
+            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
+            document.querySelector(".line").style.width = "20vw";
         }
     })
 }
@@ -37,10 +39,10 @@ const checkWin = () => {
 // Game logic
 let boxes = document.getElementsByClassName("gameBox");
 Array.from(boxes).forEach(element => {
-    let boxText = element.querySelector('.boxText');
+    let boxtext = element.querySelector('.boxText');
     element.addEventListener('click', () => {
-        if (element.innerText == '') {
-            element.innerText = turn;
+        if (boxtext.innerText == '') {
+            boxtext.innerText = turn;
             turn = changeTurn();
             if (turn === "X") {
                 xGameTurn.play();
@@ -57,4 +59,15 @@ Array.from(boxes).forEach(element => {
 })
 
 
-
+// Adding onClick Listner on click
+reset.addEventListener("click", () => {
+    let boxtexts = document.querySelectorAll(".boxText");
+    Array.from(boxtexts).forEach(element => {
+        element.innerText = ""
+    });
+    turn = "X";
+    isGameOver = false
+    document.querySelector(".line").style.width = "0vw";
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+    document.querySelector('.imgBox').getElementsByTagName('img')[0].style.width = "0px"
+})
